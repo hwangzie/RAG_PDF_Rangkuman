ac5a839874068198c5e7719e5f0cbc84e4bed151
Muhammad Zaim Shidqi <129059018+hwangzie@users.noreply.github.com>
2025-04-18 17:45:07 +0700

    Update rag.py

--- rag.py
+++ rag.py
@@ -38,7 +38,7 @@ def setup_rag(vector_store, huggingface_api_token):
     os.environ["HUGGINGFACEHUB_API_TOKEN"] = huggingface_api_token
     
     llm = HuggingFaceHub(
-        repo_id="tiiuae/falcon-7b-instruct",
+        repo_id="google/t5-v1_1-small",
         model_kwargs={"temperature": 0.5, "max_length": 512}
     )
     
